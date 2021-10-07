c324d988da95532bbddfcbf8cfdc894eef9a4daa
nmin11 <nmin1124@gmail.com>
2021-10-07 23:35:51 +0900

    ADD : Entity classes

---
+++ server/src/main/java/com/hanguseok/server/entity/Hashtag.java
@@ -0,0 +1,27 @@
+package com.hanguseok.server.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hashtag {
+
+    @Id @GeneratedValue
+    @Column(name = "hashtag_id")
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @OneToMany(mappedBy = "hashtag")
+    private List<BoardHash> reviews = new ArrayList<>();
+
+}
---
+++ server/src/main/java/com/hanguseok/server/entity/User.java
@@ -0,0 +1,36 @@
+package com.hanguseok.server.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String nickname;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String phone_number;
+
+    @OneToMany(mappedBy = "user")
+    private List<ReviewBoard> reviews = new ArrayList<>();
+
+}

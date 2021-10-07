e4b7f5360c963bcd4254fd2f3578d79dadd9d01b
skalook <skalook@naver.com>
2021-10-07 22:25:53 +0900

    UPDATE: ThemeTravel.js

--- client/src/components/ThemeTravel.js
+++ client/src/components/ThemeTravel.js
@@ -5,32 +5,201 @@ import '../styles/Theme.css';
 
 function ThemeTravel() {
   return (
-  <div>
-    <div class='hash'>
-      <h1>#신나는</h1>
-      <div class='imagebox'>
-        <img class='theme-img' src="/images/images-1.jpg"></img>
-        <img class='theme-img' src="/images/images-2.jpg"></img>
-        <img class='theme-img' src="/images/images-3.jpg"></img>
+    <div>
+      <div class='hash'>
+        <h2 class = 'themefont'>#신나는</h2>
+        <div class='imagebox'>
+          <div class="wrapper">
+            <section id="section1">
+              <a href="#section3" class="arrow__btn">‹</a>
+              <div class="item">
+                <img class='theme-img' src="/images/images-1.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-2.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-3.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-4.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-5.jpg"></img>
+              </div>
+              <a href="#section2" class="arrow__btn">›</a>
+            </section>
+            <section id="section2">
+              <a href="#section1" class="arrow__btn">‹</a>
+              <div class="item">
+                <img class='theme-img' src="/images/images-6.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-7.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-8.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-9.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-1.jpg"></img>
+              </div>
+              <a href="#section3" class="arrow__btn">›</a>
+            </section>
+            <section id="section3">
+              <a href="#section2" class="arrow__btn">‹</a>
+              <div class="item">
+                <img class='theme-img' src="/images/images-2.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-3.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-4.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-5.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-6.jpg"></img>
+              </div>
+              <a href="#section1" class="arrow__btn">›</a>
+            </section>
+          </div>
+        </div>
+      </div>
+      <div class='hash'>
+        <h2 class ='themefont'>#조용한</h2>
+        <div class='imagebox'>
+          <div class="wrapper">
+            <section id="section4">
+              <a href="#section6" class="arrow__btn">‹</a>
+              <div class="item">
+                <img class='theme-img' src="/images/images-1.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-2.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-3.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-4.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-5.jpg"></img>
+              </div>
+              <a href="#section5" class="arrow__btn">›</a>
+            </section>
+            <section id="section5">
+              <a href="#section4" class="arrow__btn">‹</a>
+              <div class="item">
+                <img class='theme-img' src="/images/images-6.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-7.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-8.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-9.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-1.jpg"></img>
+              </div>
+              <a href="#section6" class="arrow__btn">›</a>
+            </section>
+            <section id="section6">
+              <a href="#section5" class="arrow__btn">‹</a>
+              <div class="item">
+                <img class='theme-img' src="/images/images-2.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-3.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-4.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-5.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-6.jpg"></img>
+              </div>
+              <a href="#section4" class="arrow__btn">›</a>
+            </section>
+          </div>
+        </div>
+      </div>
+      <div class='hash'>
+        <h2 class='themefont'>#자연의</h2>
+        <div class='imagebox'>
+          <div class="wrapper">
+            <section id="section7">
+              <a href="#section9" class="arrow__btn">‹</a>
+              <div class="item">
+                <img class='theme-img' src="/images/images-1.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-2.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-3.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-4.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-5.jpg"></img>
+              </div>
+              <a href="#section8" class="arrow__btn">›</a>
+            </section>
+            <section id="section8">
+              <a href="#section7" class="arrow__btn">‹</a>
+              <div class="item">
+                <img class='theme-img' src="/images/images-6.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-7.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-8.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-9.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-1.jpg"></img>
+              </div>
+              <a href="#section9" class="arrow__btn">›</a>
+            </section>
+            <section id="section9">
+              <a href="#section8" class="arrow__btn">‹</a>
+              <div class="item">
+                <img class='theme-img' src="/images/images-2.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-3.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-4.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-5.jpg"></img>
+              </div>
+              <div class="item">
+                <img class='theme-img' src="/images/images-6.jpg"></img>
+              </div>
+              <a href="#section7" class="arrow__btn">›</a>
+            </section>
+          </div>
+        </div>
       </div>
     </div>
-    <div class='hash'>
-      <h1>#조용한</h1>
-      <div class='imagebox'>
-        <img class='theme-img' src="/images/images-4.jpg"></img>
-        <img class='theme-img' src="/images/images-5.jpg"></img>
-        <img class='theme-img' src="/images/images-6.jpg"></img>
-      </div>
-    </div>
-    <div class='hash'>
-      <h1>#자연의</h1>
-      <div class='imagebox'>
-        <img class='theme-img' src="/images/images-7.jpg"></img>
-        <img class='theme-img' src="/images/images-8.jpg"></img>
-        <img class='theme-img' src="/images/images-9.jpg"></img>
-      </div>
-    </div>
-  </div>
-    );
+
+  );
 }
 export default ThemeTravel;

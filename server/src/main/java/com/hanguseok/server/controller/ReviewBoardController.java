1f976960b907966b1121a74934351897602accbe
nmin11 <nmin1124@gmail.com>
2021-10-08 23:21:55 +0900

    ADD : S3 Upload code

--- server/src/main/java/com/hanguseok/server/controller/ReviewBoardController.java
+++ server/src/main/java/com/hanguseok/server/controller/ReviewBoardController.java
@@ -1,7 +1,27 @@
 package com.hanguseok.server.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.hanguseok.server.service.ReviewBoardService;
+import com.hanguseok.server.service.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
+@RequestMapping("/review")
+@RequiredArgsConstructor
 public class ReviewBoardController {
+
+    private final ReviewBoardService reviewBoardService;
+    private final S3Uploader s3Uploader;
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<?> reviewUpload(@RequestParam("data")MultipartFile multipartFile) throws IOException {
+        String uploadUrl = s3Uploader.upload(multipartFile, "static");
+        return null;
+    }
+
 }

09276ea22ea94baa3af0145e8b6ffc9af8d80d85
shreder0804 <shreder0804@gmail.com>
2021-10-09 22:04:56 +0900

    FIX : Logout Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import GlobalStyles from './components/GlobalStyles';
 import Footer from './components/Footer';
+import DestinationReviews from './components/DestinationReviews';
+import Signup from './components/Signup';
+import ThemeTravel from './components/ThemeTravel';
+import Mypage from './components/Mypage';
+import Review from './components/Review';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyles /> {/* CSS 전역 설정 */}
-      <Header /> {/* 헤더(로고, 네비게이션) */}
-      <Footer /> {/* 푸터 */}
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <GlobalStyles /> {/* CSS 전역 설정 */}
+        <Header /> {/* 헤더(로고, 네비게이션) */}
+        <Switch>
+          <Route exact path="/">
+            <ThemeTravel />
+          </Route>
+          <Route exact path="/destinationReviews">
+            <DestinationReviews />
+          </Route>
+          <Route exact path="/themeTravel">
+            <ThemeTravel />
+          </Route>
+          <Route exact path="/myPage">
+            <Mypage />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+          <Route exact path="/review">
+            <Review />
+          </Route>
+        </Switch>
+        <Footer /> {/* 푸터 */}
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;

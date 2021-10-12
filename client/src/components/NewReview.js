2d26451f164b920063122069034e7c276d6fb702
skalook <skalook@naver.com>
2021-10-12 17:51:43 +0900

    NewReview.js

--- client/src/components/NewReview.js
+++ client/src/components/NewReview.js
@@ -1,24 +1,25 @@
 import { useState } from "react";
 import axios from "axios";
-import AWS from "aws-sdk"
-
+import AWS from "aws-sdk";
+import "../styles/NewReview.css";
 axios.defaults.withCredentials = true;
 
 function NewReview() {
   const [fileInfo, setFileInfo] = useState("");
+  const [s3UploadedLink, setS3UploadedLink] = useState("");
   const [reviewData, setReviewData] = useState({
-    usertitle : "",
+    title: "",
     content: "",
     region: "",
-    hashtags: "",
+    hashtags: [],
   });
 
   AWS.config.update({
-    region: "ap-northeast-2", 
+    region: "ap-northeast-2",
     credentials: new AWS.CognitoIdentityCredentials({
       IdentityPoolId: "ap-northeast-2:c73b0ffb-7106-4208-b363-d97260804331", // cognito 인증 풀에서 받아온 키를 문자열로 입력합니다. (Ex. "ap-northeast-2...")
     }),
-  })
+  });
 
   const upload = new AWS.S3.ManagedUpload({
     params: {
@@ -26,35 +27,111 @@ function NewReview() {
       Key: fileInfo.name, // 업로드할 파일명 (* 확장자를 추가해야 합니다!)
       Body: fileInfo, // 업로드할 파일 객체
     },
-  })
+  });
 
   const fileChangeHandler = (e) => {
     setFileInfo(e.target.files[0]);
   };
   const reviewDataHandler = (key) => (e) => {
-    setReviewData({ ...reviewData, [key]: e.target.value })
-  }
-  async function reviewUploadHandler () {
-    const fd = new FormData();
-    fd.append('image', fileInfo, fileInfo.name)
-    const promise = upload.promise()
+    setReviewData({ ...reviewData, [key]: e.target.value });
+  };
+  function imageUploadHandler() {
+    const promise = upload.promise();
     promise.then(
       function (data) {
-        alert("이미지 업로드에 성공했습니다.")
+        alert("이미지 업로드에 성공했습니다.");
+        setS3UploadedLink(
+          "https://projecttt-image-bucket.s3.ap-northeast-2.amazonaws.com/" +
+            fileInfo.name
+        );
       },
       function (err) {
-        return alert("오류가 발생했습니다: ", err.message)
-      })
+        return alert("오류가 발생했습니다: ", err.message);
+      }
+    );
+  }
+  const hashtagHandler = (e) => {
+    if (e.target.value[0] !== "#") {
+      e.target.value = "#" + e.target.value;
+    }
+    if (e.key === "Enter") {
+      reviewData.hashtags.push(e.target.value.slice(1));
+      e.target.value = "";
+    }
   };
+  async function reviewUploadHandler() {
+    await axios(
+      "http://ec2-3-35-140-107.ap-northeast-2.compute.amazonaws.com:8080/review",
+      {
+        method: "POST",
+        data: {
+          image: s3UploadedLink,
+          userId: 36,
+          title: reviewData.title,
+          content: reviewData.content,
+          region: reviewData.region,
+          hashtags: ["재미있는", "신나는", "조용한"],
+        },
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Credentials": "true",
+        },
+        withCredentials: true,
+      }
+    )
+      .then((res) => {})
+      .catch((e) => {});
+  }
+
   return (
     <div>
       <div>
         <input type="file" onChange={fileChangeHandler} />
-        <button onClick={reviewUploadHandler}>글쓰기</button>
+        <button onClick={imageUploadHandler}>이미지 업로드</button>
       </div>
       <div>
-        <textarea maxLength="1500" rows="10" cols="50" onChange={reviewDataHandler('content')}></textarea>
+        <textarea
+          maxLength="40"
+          rows="1"
+          cols="40"
+          onChange={reviewDataHandler("title")}
+        ></textarea>
       </div>
+      <div>
+        <textarea
+          maxLength="1500"
+          rows="10"
+          cols="50"
+          onChange={reviewDataHandler("content")}
+        ></textarea>
+      </div>
+      <div>
+        <input
+          className="small-textarea"
+          placeholder="해쉬태그를 작성해주세요"
+          maxLength="15"
+          size="40"
+          onChange={hashtagHandler}
+          onKeyPress={hashtagHandler}
+        ></input>
+        <div>
+          {reviewData.hashtags.map((ele) => {
+            return "#" + ele + " ";
+          })}
+        </div>
+      </div>
+      <div>
+        <input
+          className="small-textarea"
+          placeholder="여행지를 적어주세요"
+          maxLength="15"
+          size="15"
+          onChange={reviewDataHandler("region")}
+        ></input>
+      </div>
+      <button onClick={reviewUploadHandler}>글쓰기</button>
     </div>
   );
 }

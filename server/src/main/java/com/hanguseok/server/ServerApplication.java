f4a12adde14d4c10789a21262dab8bee1ded51f9
nmin11 <nmin1124@gmail.com>
2021-10-12 23:29:45 +0900

    FIX : N:N edit logic

--- server/src/main/java/com/hanguseok/server/ServerApplication.java
+++ server/src/main/java/com/hanguseok/server/ServerApplication.java
@@ -9,14 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class ServerApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "classpath:aws.yml";
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ServerApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }

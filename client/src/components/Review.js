71610352ad43a31dd33ffcb3be722b553c348111
skalook <skalook@naver.com>
2021-10-13 11:52:38 +0900

    UPDATE:Review.js

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,67 +1,97 @@
-import React, { useState }  from 'react';
-import { useLocation } from 'react-router';
-import { Link, useHistory } from 'react-router-dom';
-import { dummyComments } from '../dummy/dummyData';
-import axios from 'axios';
-import '../styles/Review.css';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+import "../styles/Review.css";
 
 axios.defaults.withCredentials = true;
 
-function Review() {
+function Review(props) {
+
   const history = useHistory();
-  const { state , props } = useLocation();
-  const [comment, setComment] = useState('');
-  const changeComment = (e) => {
-    setComment(e.target.value);
-  }
-  async function deleteReview() {
+  const { state } = useLocation();
+
+  const [newComment, setNewComment] = useState("1");
+  const [commentText, setCommentText] = useState("");
+  const [comment, setComment] = useState([])
+  const changeCommentText = (e) => {
+    setCommentText(e.target.value);
+  };
+
+  useEffect(() => {
+    getComments();
+  }, [newComment]);
+  async function getComments() {
     await axios(
-      'http://ec2-3-35-140-107.ap-northeast-2.compute.amazonaws.com:8080/review/' + state.id,
+      "http://ec2-3-35-140-107.ap-northeast-2.compute.amazonaws.com:8080/review/" + state.id,
       {
-        method: 'DELETE',
+        method: "GET",
         headers: {
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'DELETE',
-          'Access-Control-Allow-Credentials': 'true',
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET",
+          "Access-Control-Allow-Credentials": "true",
         },
         withCredentials: true,
       }
     )
       .then((res) => {
-        history.push('/destinationReviews')
+        let commentObj = res.data.comments;
+        let objectToArray = [];
+        let keys = Object.keys(commentObj);
+        for (let i = 0; i < keys.length; i++) {
+          objectToArray.push({ ...commentObj[keys[i]], id: keys[i] });
+        }
+        setComment(objectToArray);
       })
-      .catch((e) => {
-
-      });
+      .catch((e) => {});
+  }
+  console.log(props.userInfo)
+  async function deleteReview() {
+    await axios(
+      "http://ec2-3-35-140-107.ap-northeast-2.compute.amazonaws.com:8080/review/" +
+        state.id,
+      {
+        method: "DELETE",
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "DELETE",
+          "Access-Control-Allow-Credentials": "true",
+        },
+        withCredentials: true,
+      }
+    )
+      .then((res) => {
+        history.push("/destinationReviews");
+      })
+      .catch((e) => {});
   }
   async function postComment() {
     await axios(
-      'http://ec2-3-35-140-107.ap-northeast-2.compute.amazonaws.com:8080/comment',
+      "http://ec2-3-35-140-107.ap-northeast-2.compute.amazonaws.com:8080/comment",
       {
-        method: 'POST',
+        method: "POST",
         data: {
-          userId : 1,
-          reviewId : state.id,
-          content : comment
+          userId: props.userInfo.id,
+          reviewId: state.id,
+          content: commentText,
         },
         headers: {
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST',
-          'Access-Control-Allow-Credentials': 'true',
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Credentials": "true",
         },
         withCredentials: true,
       }
     )
       .then((res) => {
-
+        setCommentText("")
+        setNewComment(newComment+1)
       })
-      .catch((e) => {
-
-      });
-  }
-
+      .catch((e) => {});
+    }
   return (
     <div id="review-header">
       <div id="review-title">{state.title}</div>
@@ -72,37 +102,47 @@ function Review() {
       <img id="review-img" src={state.image} alt={state.image}></img>
       <div id="reviewer-description">{state.content}</div>
       <span>
-      {props.loginOn ? 
-      <Link
-        to={{
-          pathname: '/newReview',
-          props: props,
-          state: state,
-          mode: "modifyPost",
-        }}
-      >
-        <button id="review-modify-btn">글수정</button>
-      <button onClick={deleteReview}>글삭제(테스트용 아무나 삭제 가능)</button>
-      </Link>
-      : null}
+        {props.loginOn ? (
+          <Link
+            to={{
+              pathname: "/newReview",
+              props: props,
+              state: state,
+              mode: "modifyPost",
+            }}
+          >
+            <button id="review-modify-btn">글수정</button>
+            <button onClick={deleteReview}>
+              글삭제(테스트용 아무나 삭제 가능)
+            </button>
+          </Link>
+        ) : null}
       </span>
 
       <div>
-        {dummyComments.map((ele) => {
-          if (ele.board_id === state.id) {
+        {comment.length !== 0 ? comment.map((ele) => {
             return (
               <div class="comment-content">
-                <span class="comment-user">{`댓글 작성자`}</span>
-                <span class="comment-createdat">{ele.created_at.toString()}</span>
-                <span class="comment-description">{ele.content}</span>
+                <span class="comment-user">{ele['comment-writer']}</span>
+                <span class="comment-createdat">
+                </span>
+                <span class="comment-description">{ele['comment-content']}</span>
               </div>
             );
           }
-        })}
+        ) : null}
       </div>
       <div>
-        <span>댓글작성자</span>
-        <textarea maxLength="500" rows="5" cols="60" onChange={changeComment}></textarea>
+      {props.loginOn ?
+        <span>{}</span> : <span>로그인이 필요합니다</span>
+      }
+        <textarea
+          maxLength="500"
+          rows="5"
+          cols="60"
+          value={commentText}
+          onChange={changeCommentText}
+        ></textarea>
         <button onClick={postComment}>등록</button>
       </div>
     </div>

c8785035b61222af7c66a782e75e64138f707a1c
skalook <skalook@naver.com>
2021-10-13 22:32:01 +0900

    UPDATE:DestinationReveiw.js

--- client/src/components/DestinationReviews.js
+++ client/src/components/DestinationReviews.js
@@ -34,6 +34,9 @@ function DestinationReviews(props) {
         for (let i = 0; i < keys.length; i++) {
           objectToArray.push({ ...reviewObj[keys[i]], id: keys[i] });
         }
+        objectToArray.sort(function(a, b){
+          return b.recommend - a.recommend
+        });
         setReviews(objectToArray);
       })
       .catch((e) => {});

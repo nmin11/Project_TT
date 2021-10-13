8c922b58aa99d00e94a22422056b2ba5fd2f46fc
nmin11 <nmin1124@gmail.com>
2021-10-13 10:00:36 +0900

    EDIT : get profile logic

--- server/src/main/java/com/hanguseok/server/service/TokenService.java
+++ server/src/main/java/com/hanguseok/server/service/TokenService.java
@@ -17,14 +17,13 @@ public class TokenService {
     private final static String KEY = "hanguseokkey";
 
     public String createJwtToken(User user, Long time) {
-        Date expireDate = new Date(new Date().getTime() + time);
         Date now = new Date();
 
         return Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .setIssuer("fresh")
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + Duration.ofSeconds(time).toMillis()))
+                .setExpiration(new Date(now.getTime() + Duration.ofHours(time).toMillis()))
                 .claim("email", user.getEmail())
                 .claim("password", user.getPassword())
                 .signWith(SignatureAlgorithm.HS256, KEY)
